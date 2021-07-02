9b67be21fa38ed5a81bbad08fa195812124fd761
Jonathan Bessette <JOBES10@ulaval.ca>
2021-07-02 15:03:00 -0700

    F

---
+++ header.js
@@ -0,0 +1,63 @@
+// HEADER PROJECT
+$( "#h-menu-tproject" ).mouseenter(function() {
+    $('#h-menu-dproject').removeClass("h-C2-dexpand-200");
+    $('#h-menu-dproject').addClass("h-C2-expand-200");
+  });
+$( ".h-menu-project" ).mouseleave(function() {
+    $('#h-menu-dproject').removeClass("h-C2-expand-200");
+    $('#h-menu-dproject').addClass("h-C2-dexpand-200");
+  });
+// HEADER TEAM
+$( "#h-menu-tteam" ).mouseenter(function() {
+    $('#h-menu-dteam').removeClass("h-C2-dexpand-200");
+    $('#h-menu-dteam').addClass("h-C2-expand-200");
+  });
+$( ".h-menu-team" ).mouseleave(function() {
+    $('#h-menu-dteam').removeClass("h-C2-expand-200");
+    $('#h-menu-dteam').addClass("h-C2-dexpand-200");
+  });
+// HEADER PARTS
+$( "#h-menu-tparts" ).mouseenter(function() {
+    $('#h-menu-dparts').removeClass("h-C2-dexpand-200");
+    $('#h-menu-dparts').addClass("h-C2-expand-200");
+  });
+$( ".h-menu-parts" ).mouseleave(function() {
+    $('#h-menu-dparts').removeClass("h-C2-expand-200");
+    $('#h-menu-dparts').addClass("h-C2-dexpand-200");
+  });
+// HEADER AWARDS
+  $( "#h-menu-tawards" ).mouseenter(function() {
+    $('#h-menu-dawards').removeClass("h-C2-dexpand-200");
+    $('#h-menu-dawards').addClass("h-C2-expand-200");
+  });
+$( ".h-menu-awards" ).mouseleave(function() {
+    $('#h-menu-dawards').removeClass("h-C2-expand-200");
+    $('#h-menu-dawards').addClass("h-C2-dexpand-200");
+  });
+// HEADER SAFETY
+$( "#h-menu-tsafety" ).mouseenter(function() {
+    $('#h-menu-dsafety').removeClass("h-C2-dexpand-200");
+    $('#h-menu-dsafety').addClass("h-C2-expand-200");
+  });
+$( ".h-menu-safety" ).mouseleave(function() {
+    $('#h-menu-dsafety').removeClass("h-C2-expand-200");
+    $('#h-menu-dsafety').addClass("h-C2-dexpand-200");
+  });
+// HEADER POSTER
+$( "#h-menu-tposter" ).mouseenter(function() {
+    $('#h-menu-dposter').removeClass("h-C2-dexpand-200");
+    $('#h-menu-dposter').addClass("h-C2-expand-200");
+  });
+$( ".h-menu-poster" ).mouseleave(function() {
+    $('#h-menu-dposter').removeClass("h-C2-expand-200");
+    $('#h-menu-dposter').addClass("h-C2-dexpand-200");
+  });
+// HEADER HUMAN-PRACTICES
+$( "#h-menu-thumanpractices" ).mouseenter(function() {
+    $('#h-menu-dhumanpractices').removeClass("h-C2-dexpand-200");
+    $('#h-menu-dhumanpractices').addClass("h-C2-expand-200");
+  });
+$( ".h-menu-humanpractices" ).mouseleave(function() {
+    $('#h-menu-dhumanpractices').removeClass("h-C2-expand-200");
+    $('#h-menu-dhumanpractices').addClass("h-C2-dexpand-200");
+  });
