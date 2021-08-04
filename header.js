469911df0db52540f6bebc4515f5152428d079aa
Jonathan Bessette <JOBES10@ulaval.ca>
2021-08-04 09:03:43 -0700

    +Attributions

--- header.js
+++ header.js
@@ -1,65 +1,79 @@
 // HEADER PROJECT
 $( "#h-menu-tproject" ).mouseenter(function() {
-    $('#h-menu-dproject').removeClass("h-C2-dexpand-200");
-    $('#h-menu-dproject').addClass("h-C2-expand-200");
+    $('#h-menu-dproject').animate({
+      height: 200
+    }, 300)
   });
 $( ".h-menu-project" ).mouseleave(function() {
-    $('#h-menu-dproject').removeClass("h-C2-expand-200");
-    $('#h-menu-dproject').addClass("h-C2-dexpand-200");
+  $('#h-menu-dproject').animate({
+    height: 0
+  }, 300)
   });
 // HEADER TEAM
 $( "#h-menu-tteam" ).mouseenter(function() {
-    $('#h-menu-dteam').removeClass("h-C2-dexpand-200");
-    $('#h-menu-dteam').addClass("h-C2-expand-200");
+  $('#h-menu-dteam').animate({
+    height: 200
+  }, 300)
   });
 $( ".h-menu-team" ).mouseleave(function() {
-    $('#h-menu-dteam').removeClass("h-C2-expand-200");
-    $('#h-menu-dteam').addClass("h-C2-dexpand-200");
+  $('#h-menu-dteam').animate({
+    height: 0
+  }, 300)
   });
 // HEADER PARTS
 $( "#h-menu-tparts" ).mouseenter(function() {
-    $('#h-menu-dparts').removeClass("h-C2-dexpand-200");
-    $('#h-menu-dparts').addClass("h-C2-expand-200");
+  $('#h-menu-dparts').animate({
+    height: 200
+  }, 300)
   });
 $( ".h-menu-parts" ).mouseleave(function() {
-    $('#h-menu-dparts').removeClass("h-C2-expand-200");
-    $('#h-menu-dparts').addClass("h-C2-dexpand-200");
+  $('#h-menu-dparts').animate({
+    height: 0
+  }, 300)
   });
 // HEADER AWARDS
   $( "#h-menu-tawards" ).mouseenter(function() {
-    $('#h-menu-dawards').removeClass("h-C2-dexpand-200");
-    $('#h-menu-dawards').addClass("h-C2-expand-200");
-  });
+    $('#h-menu-dawards').animate({
+      height: 200
+    }, 300)
+    });
 $( ".h-menu-awards" ).mouseleave(function() {
-    $('#h-menu-dawards').removeClass("h-C2-expand-200");
-    $('#h-menu-dawards').addClass("h-C2-dexpand-200");
+  $('#h-menu-dawards').animate({
+    height: 0
+  }, 300)
   });
 // HEADER SAFETY
 $( "#h-menu-tsafety" ).mouseenter(function() {
-    $('#h-menu-dsafety').removeClass("h-C2-dexpand-200");
-    $('#h-menu-dsafety').addClass("h-C2-expand-200");
+  $('#h-menu-dsafety').animate({
+    height: 200
+  }, 300)
   });
 $( ".h-menu-safety" ).mouseleave(function() {
-    $('#h-menu-dsafety').removeClass("h-C2-expand-200");
-    $('#h-menu-dsafety').addClass("h-C2-dexpand-200");
+  $('#h-menu-dsafety').animate({
+    height: 0
+  }, 300)
   });
 // HEADER POSTER
 $( "#h-menu-tposter" ).mouseenter(function() {
-    $('#h-menu-dposter').removeClass("h-C2-dexpand-200");
-    $('#h-menu-dposter').addClass("h-C2-expand-200");
+  $('#h-menu-dposter').animate({
+    height: 200
+  }, 300)
   });
 $( ".h-menu-poster" ).mouseleave(function() {
-    $('#h-menu-dposter').removeClass("h-C2-expand-200");
-    $('#h-menu-dposter').addClass("h-C2-dexpand-200");
+  $('#h-menu-dposter').animate({
+    height: 0
+  }, 300)
   });
 // HEADER HUMAN-PRACTICES
 $( "#h-menu-thumanpractices" ).mouseenter(function() {
-    $('#h-menu-dhumanpractices').removeClass("h-C2-dexpand-200");
-    $('#h-menu-dhumanpractices').addClass("h-C2-expand-200");
+  $('#h-menu-dhumanpractices').animate({
+    height: 200
+  }, 300)
   });
 $( ".h-menu-humanpractices" ).mouseleave(function() {
-    $('#h-menu-dhumanpractices').removeClass("h-C2-expand-200");
-    $('#h-menu-dhumanpractices').addClass("h-C2-dexpand-200");
+  $('#h-menu-dhumanpractices').animate({
+    height: 0
+  }, 300)
   });
 
 
