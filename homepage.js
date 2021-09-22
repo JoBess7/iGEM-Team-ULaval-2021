178ac5dbd74e710eb9e2115627050caba7d10a85
JoBess7 <jonathan.bessette.1@ulaval.ca>
2021-09-22 03:43:15 -0400

    Homepage - structure completed

--- homepage.js
+++ homepage.js
@@ -4,14 +4,14 @@ setInterval(() => {
     thatMeansDotCounter += 1;
     thatMeans.innerHTML += ".";
     if(thatMeansDotCounter === 4) {
-        thatMeans.innerHTML = "That means "
+        thatMeans.innerHTML = "That means ";
         thatMeansDotCounter = 0;
     }
 }, 600);
 
 
 var didScrollHomepage;
-$(document).scroll(function() {
+$(window).scroll(function() {
     didScrollHomepage = true;
 })
 
@@ -20,8 +20,11 @@ const scrollInterval = setInterval(() => {
         checkPagePosition();
         didScrollHomepage = false;
     }
-    if(Text1OpacityHasLaunched === true && PourcentageHasLaunched === true) {
-        clearInterval(scrollInterval);
+    if(Text1OpacityHasLaunched === true) 
+    {
+        if(PourcentageHasLaunched === true) {
+            clearInterval(scrollInterval);
+        }
     }
 }, 300);
 
@@ -67,3 +70,75 @@ function checkVisible(elm) {
     var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
     return !(rect.bottom < 0 || rect.top - viewHeight >= 0) 
 }
+
+var bodyRect;
+var elementRect;
+var offset;
+var goutteContainer = document.getElementById("goutte-container");
+var entaille = document.getElementById("entaille");
+var entailleHeight;
+$(window).scroll(function (e) {
+    bodyRect = document.body.getBoundingClientRect();
+    elemRect = entaille.getBoundingClientRect();
+    offset   = elemRect.top - bodyRect.top;
+    entailleHeight = entaille.clientHeight;
+    var pos = entailleHeight + offset;
+    if ($(window).scrollTop() > pos - 45) {
+        $("#goutte").css({
+            position: 'fixed',
+            top: 45,
+            height: entaille.clientHeight * 2 
+        });
+    } else {
+        $("#goutte").css({
+            position: "absolute",
+            top: goutteContainer.clientHeight * 0.7 + 430,
+            height: entaille.clientHeight * 2
+        });
+    }
+});
+
+$(window).resize(function () {
+    bodyRect = document.body.getBoundingClientRect(),
+    elemRect = entaille.getBoundingClientRect(),
+    offset   = elemRect.top - bodyRect.top;
+    entailleHeight = entaille.clientHeight;
+    var pos = entailleHeight + offset;
+    if ($(window).scrollTop() > pos - 45) {
+        $("#goutte").css({
+            position: 'fixed',
+            top: 45,
+            height: entaille.clientHeight * 2
+        });
+    } else {
+        $("#goutte").css({
+            position: "absolute",
+            top: goutteContainer.clientHeight * 0.7 + 430,
+            height: entaille.clientHeight * 2
+        });
+    }
+})
+
+var clickables = [
+    "#clickable-project",
+    "#clickable-team",
+    "#clickable-awards",
+    "#clickable-safety",
+    "#clickable-parts",
+    "#clickable-humanpractices"
+]
+
+clickables.map((item, index) => {
+    $( item ).mouseenter(function() {
+        $(item).animate({
+            width: 250, height: 310}, {
+            duration: 100
+        })  
+      });
+    $( item ).mouseleave(function() {
+        $(item).animate({
+            width: 240, height: 300}, {
+            duration: 100
+        })  
+      });
+  })
